799d9a72e2c30c713ee1580e19053e2c2a95fa94
Tomas Padilla <padillatomasagustin@gmail.com>
2020-08-06 19:32:03 +0200

    VER NAVBAR!!

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 $(document).ready(function () {
-
   //toggle button
-  $('.btn').click(function () {
-    $('.nav-links').slideToggle(3000);
-    $('.btn').toggleClass('change');
-
-  })
+  $('.hamburger-btn').click(function () {
+    $('.nav-links').slideToggle(800);
+    $('.hamburger-btn').toggleClass('change');
+  });
   //transparent background
 
   $(window).scroll(function () {
-
     let position = $(window).scrollTop();
 
-
     if (position >= 100) {
-      $('nav, nav-container').addClass('navBackground')
+      $('nav, nav-container').addClass('navBackground');
     } else {
-      $('nav, nav-container').removeClass('navBackground')
-
+      $('nav, nav-container').removeClass('navBackground');
     }
-
-  })
+  });
   //smooth scroll
 
   $('.nav-links a').click(function (link) {
     link.preventDefault();
 
     let target = $(this).attr('href');
-    $('html, body').stop().animate({
-      scrollTop: $(target).offset().top
-    }, 3000);
-
-  })
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        3000
+      );
+  });
   //owl team
   $('.team-center').owlCarousel({
     loop: true,
@@ -40,16 +38,16 @@ $(document).ready(function () {
     nav: true,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       600: {
-        items: 3
+        items: 3,
       },
       1000: {
-        items: 5
-      }
-    }
-  })
+        items: 5,
+      },
+    },
+  });
   //owl customers
   $('.customers-center').owlCarousel({
     loop: true,
@@ -58,15 +56,14 @@ $(document).ready(function () {
     nav: true,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       600: {
-        items: 1
+        items: 1,
       },
       1000: {
-        items: 3
-      }
-    }
-  })
-
-})
+        items: 3,
+      },
+    },
+  });
+});
